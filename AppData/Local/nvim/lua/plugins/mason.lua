eb762eb838eddc861fe17c34aaf782e36227b354
seolman <tjfehdgns@gmail.com>
2025-02-19 17:18:24 +0900

    .

--- AppData/Local/nvim/lua/plugins/mason.lua
+++ AppData/Local/nvim/lua/plugins/mason.lua
@@ -1,1 +1,20 @@
-return {}
+return {
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+  },
+  config = function()
+    require("mason").setup()
+    require("mason-lspconfig").setup({
+      ensure_installed = {
+        "bashls",
+      },
+      handlers = {
+        function(server_name)
+          require("lspconfig")[server_name].setup({})
+        end,
+      }
+    })
+  end,
+}
